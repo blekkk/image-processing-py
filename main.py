a66b994fb46f86e7db8dfebe1736463c51e67d1e
blekkk <fazailman992@gmail.com>
2021-11-01 21:13:22 +0700

    histogram

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from menubars import MenuBars
 from utils import image_resize
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class App:
   def __init__(self, window, window_title, window_size="1920x1080"):
@@ -14,9 +17,13 @@ class App:
     self.window.option_add('*tearOff', FALSE)
 
     # Initialize panel for displaying image
-    self.panelLeft = None
-    self.panelRight = None
+    self.labelLeft = None
+    self.labelRight = None
+    self.canvasRight = None
     self.size = [1000, 1000]
+    self.displayMode = 1
+    self.histogramFigure = plt.Figure(figsize=(4,4), dpi=100)
+    self.histogramCanvas = FigureCanvasTkAgg(self.histogramFigure, self.window)
 
     MenuBars(self)
 
@@ -27,79 +34,79 @@ class App:
     return filename
 
   def open_img(self):
-    x = self.openfn()
-    self.img = cv2.cvtColor(cv2.imread(x), cv2.COLOR_BGR2RGB)
-    self.img2 = self.img
-    self.photo = Image.fromarray(self.img)
+    self.x = self.openfn()
+    self.ORIGINAL_IMAGE = cv2.cvtColor(cv2.imread(self.x), cv2.COLOR_BGR2RGB)
+    self.modifiableImage = cv2.cvtColor(cv2.imread(self.x), cv2.COLOR_BGR2RGB)
+    self.photo = Image.fromarray(self.ORIGINAL_IMAGE)
     self.photo.thumbnail(self.size, Image.ANTIALIAS)
     self.photo = ImageTk.PhotoImage(image = self.photo)
     
-    if(self.panelLeft != None and self.panelRight != None):
-      self.panelLeft.configure(image=self.photo)
-      self.panelLeft.image = self.photo
-      self.panelRight.configure(image=self.photo)
-      self.panelRight.image = self.photo
+    if(self.labelLeft != None and self.labelRight != None):
+      self.labelLeft.configure(image=self.photo)
+      self.labelLeft.image = self.photo
+      self.labelRight.configure(image=self.photo)
+      self.labelRight.image = self.photo
     else:
-      self.panelLeft = Label(self.window, image=self.photo)
-      self.panelLeft.image = self.photo
-      self.panelLeft.pack(side="left", padx=10, pady=10)
-      self.panelRight = Label(self.window, image=self.photo)
-      self.panelRight.image = self.photo
-      self.panelRight.pack(side="right", padx=10, pady=10)
+      self.labelLeft = Label(self.window, image=self.photo)
+      self.labelLeft.image = self.photo
+      self.labelLeft.pack(side="left", padx=10, pady=10)
+      self.labelRight = Label(self.window, image=self.photo)
+      self.labelRight.image = self.photo
+      self.labelRight.pack(side="right", padx=10, pady=10)
 
   def scaleImage(self, percent):
     scale_percent = percent
-    self.height, self.width, no_channels = self.img2.shape
+    self.height, self.width, no_channels = self.modifiableImage.shape
     self.width = int(self.width * (scale_percent / 100))
     self.height = int(self.height * (scale_percent / 100))
     dim = (self.width, self.height)
     print(dim)
       
-    npImg = np.array(self.img2)
+    npImg = np.array(self.modifiableImage)
     resized = cv2.resize(npImg, dim, interpolation = cv2.INTER_NEAREST)
-    self.img2 = resized
+    self.modifiableImage = resized
     resized = Image.fromarray(resized)
     resized = ImageTk.PhotoImage(resized)
-    self.panelRight.configure(image=resized)
-    self.panelRight.image = resized
+    self.labelRight.configure(image=resized)
+    self.labelRight.image = resized
 
   def downSampling(self):
-    if(self.img2.shape[0] > 1000 and self.img2.shape[1] > 1000):
-      self.img2 = image_resize(self.img2, height = 1000)
+    if(self.modifiableImage.shape[0] > 1000 and self.modifiableImage.shape[1] > 1000):
+      self.modifiableImage = image_resize(self.modifiableImage, height = 1000)
 
-    height, width = self.img2.shape[0], self.img2.shape[1]
+    height, width = self.modifiableImage.shape[0], self.modifiableImage.shape[1]
     new_h, new_w = int(height/2), int(width/2)
 
-    self.img2 = image_resize(self.img2, new_w, new_h)
-    self.img2 = image_resize(self.img2, height = 1000)
+    self.modifiableImage = image_resize(self.modifiableImage, new_w, new_h)
+    self.modifiableImage = image_resize(self.modifiableImage, height = 1000)
 
-    self.displayOnRightPanel(self.img2)
+    self.displayImage()
 
   def quantization(self):
-    height, width = self.img2.shape[0], self.img2.shape[1]
+    height, width = self.modifiableImage.shape[0], self.modifiableImage.shape[1]
     new_img = np.zeros((height, width, 3), np.uint8)
 
     #  Image quantization operation , The quantification level is 2
     for i in range(height):
         for j in range(width):
             for k in range(3):  #  Correspondence BGR Three channels 
-                if self.img2[i, j][k] < 128:
+                if self.modifiableImage[i, j][k] < 128:
                     gray = 0
                 else:
                     gray = 129
                 new_img[i, j][k] = np.uint8(gray)
-    self.img2 = new_img
-    self.displayOnRightPanel(self.img2)
+    self.modifiableImage = new_img
+    self.displayImage()
 
   def invertNegative(self):
-    self.img2[:,:,0] = 255 - self.img2[:,:,0]
-    self.img2[:,:,1] = 255 - self.img2[:,:,1]
-    self.img2[:,:,2] = 255 - self.img2[:,:,2]
+    self.modifiableImage[:,:,0] = 255 - self.modifiableImage[:,:,0]
+    self.modifiableImage[:,:,1] = 255 - self.modifiableImage[:,:,1]
+    self.modifiableImage[:,:,2] = 255 - self.modifiableImage[:,:,2]
 
-    self.displayOnRightPanel(self.img2)
+    self.displayImage()
 
   def modifyBrightness(self, value=30):
-    hsv = cv2.cvtColor(self.img2, cv2.COLOR_RGB2HSV)
+    hsv = cv2.cvtColor(self.modifiableImage, cv2.COLOR_RGB2HSV)
     h, s, v = cv2.split(hsv)
 
     if value >= 0:
@@ -113,20 +120,69 @@ class App:
       v[v >= lim] -= value
 
     final_hsv = cv2.merge((h, s, v))
-    self.img2 = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2RGB)
+    self.modifiableImage = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2RGB)
 
-    self.displayOnRightPanel(self.img2)
+    self.displayImage()
 
   def clearEdits(self):
-    self.img2 = self.img
-    self.displayOnRightPanel(self.img2)
+    self.modifiableImage = cv2.cvtColor(cv2.imread(self.x), cv2.COLOR_BGR2RGB)
+    self.displayImage()
 
-  def displayOnRightPanel(self, image):
+  def displayOnLeftLabel(self, image):
+    self.photo2 = Image.fromarray(image)
+    self.photo2.thumbnail(self.size, Image.ANTIALIAS)
+    self.photo2 = ImageTk.PhotoImage(image = self.photo2)
+
+    self.labelLeft.configure(image=self.photo2)
+    self.labelLeft.image = self.photo2 
+
+  def displayOnRightLabel(self, image):
     self.photo = Image.fromarray(image)
     self.photo.thumbnail(self.size, Image.ANTIALIAS)
     self.photo = ImageTk.PhotoImage(image = self.photo)
 
-    self.panelRight.configure(image=self.photo)
-    self.panelRight.image = self.photo 
+    self.labelRight.configure(image=self.photo)
+    self.labelRight.image = self.photo
+
+  def displayImage(self):
+    if self.displayMode == 1:
+      self.displayOnLeftLabel(self.ORIGINAL_IMAGE)
+      self.displayOnRightLabel(self.modifiableImage)
+    elif self.displayMode == 2:
+      self.displayOnLeftLabel(self.modifiableImage)
+      self.showHistogram(self.modifiableImage)
+
+  def changeDisplayMode(self, mode):
+    self.displayMode = mode
+    if self.displayMode == 1:
+      self.histogramCanvas.get_tk_widget().pack_forget()
+      self.labelRight = Label(self.window, image=self.photo)
+      self.labelRight.image = self.photo
+      self.labelRight.pack(side="right", padx=10, pady=10)
+    elif self.displayMode == 2:
+      self.labelRight.pack_forget()
+    self.displayImage()
+  
+  def showHistogram(self, image):
+    self.histogramCanvas.get_tk_widget().pack_forget()
+    subplot = 221
+
+    self.histogramFigure = plt.Figure(figsize=(4,4), dpi=100)
+
+    for i, color in enumerate(['r', 'g', 'b']):
+      self.histogramFigure.add_subplot(subplot).plot(cv2.calcHist([image],[i],None,[256],[0,256]), color = color)
+      subplot = subplot + 1
+    
+    self.histogramCanvas = FigureCanvasTkAgg(self.histogramFigure, self.window)
+    self.histogramCanvas.get_tk_widget().pack(side="right")
+
+  def equalize(self):
+    channels = cv2.split(self.modifiableImage)
+    eq_channels = []
+    for ch, color in zip(channels, ['R', 'G', 'B']):
+        eq_channels.append(cv2.equalizeHist(ch))
+
+    self.modifiableImage = cv2.merge(eq_channels)
+    self.displayImage()
 
 App(Tk(), "Image Processing")
--- menubars.py
+++ menubars.py
@@ -4,24 +4,30 @@ class MenuBars:
   def __init__(self, window):
     window.m = Menu(window.window)
     
-    window.file_menu = Menu(window.m)
-    window.edit_menu = Menu(window.m)
-    window.image_submenu = Menu(window.m)
+    window.menu_file = Menu(window.m)
+    window.menu_edit = Menu(window.m)
+    window.menu_mode = Menu(window.m)
+    window.submenu_image = Menu(window.m)
 
-    window.m.add_cascade(menu=window.file_menu, label="File")
-    window.m.add_cascade(menu=window.edit_menu, label="Edit")
+    window.m.add_cascade(menu=window.menu_file, label="File")
+    window.m.add_cascade(menu=window.menu_edit, label="Edit")
+    window.m.add_cascade(menu=window.menu_mode, label="Mode")
 
-    window.file_menu.add_cascade(label="Image", menu=window.image_submenu)
-    window.image_submenu.add_command(label="New Image", command=window.open_img)
+    window.menu_file.add_cascade(label="Image", menu=window.submenu_image)
+    window.submenu_image.add_command(label="New Image", command=window.open_img)
 
-    window.edit_menu.add_cascade(label="Downscale", command= lambda: window.scaleImage(50))
-    window.edit_menu.add_cascade(label="Upscale", command= lambda: window.scaleImage(200))
-    window.edit_menu.add_cascade(label="Down Sample", command=window.downSampling)
-    window.edit_menu.add_cascade(label="Quantisize", command=window.quantization)
-    window.edit_menu.add_cascade(label="Negative", command=window.invertNegative)
-    window.edit_menu.add_cascade(label="Increase Brightness", command=lambda: window.modifyBrightness(30))
-    window.edit_menu.add_cascade(label="Decrease Brightness", command=lambda: window.modifyBrightness(-30))
-    window.edit_menu.add_cascade(label="Clear Edits", command=window.clearEdits)
+    window.menu_edit.add_cascade(label="Downscale", command= lambda: window.scaleImage(50))
+    window.menu_edit.add_cascade(label="Upscale", command= lambda: window.scaleImage(200))
+    window.menu_edit.add_cascade(label="Down Sample", command=window.downSampling)
+    window.menu_edit.add_cascade(label="Quantisize", command=window.quantization)
+    window.menu_edit.add_cascade(label="Negative", command=window.invertNegative)
+    window.menu_edit.add_cascade(label="Increase Brightness", command=lambda: window.modifyBrightness(30))
+    window.menu_edit.add_cascade(label="Decrease Brightness", command=lambda: window.modifyBrightness(-30))
+    window.menu_edit.add_cascade(label="Equalize", command=window.equalize)
+    window.menu_edit.add_cascade(label="Clear Edits", command=window.clearEdits)
+
+    window.menu_mode.add_cascade(label="Mode 1", command=lambda: window.changeDisplayMode(1))
+    window.menu_mode.add_cascade(label="Mode 2", command=lambda: window.changeDisplayMode(2))
 
     window.window['menu'] = window.m
 
